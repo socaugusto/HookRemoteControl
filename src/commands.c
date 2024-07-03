dcd9b98a78c3632c295af680001869159e74c711
Silvio Augusto <sa@staveng.no>
2024-07-03 18:51:16 +0200

    Add missing break in command.c

--- src/commands.c
+++ src/commands.c
@@ -102,10 +102,12 @@ void command_run(void)
             cmdObject.operation = cmd->operation;
             cmdObject.task = executeCmdMidOpen;
 
+            break;
         case COMMAND_ENABLE_RECOVERY:
             cmdObject.operation = cmd->operation;
             cmdObject.task = executeEnableRecovery;
 
+            break;
         default:
 
             break;
