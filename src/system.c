a09d40871ce05b4859dd45be455fc82f3308b1e6
Silvio Augusto <sa@staveng.no>
2024-09-05 23:22:11 +0200

    Disable remote and command while disconnected

--- src/system.c
+++ src/system.c
@@ -5,6 +5,9 @@
 #include "database.h"
 #include "commands.h"
 
+static int32_t connection = 0;
+static int32_t enableTimer = 6; // 6 * 500ms
+
 void system_init(const void *lcd_dev, const void *cs_dev)
 {
     comm_init();
@@ -15,8 +18,11 @@ void system_init(const void *lcd_dev, const void *cs_dev)
 void system_thread(void)
 {
     database_run();
-    remote_run();
-    command_run();
+    if (connection >= enableTimer) // 3s connected
+    {
+        remote_run();
+        command_run();
+    }
 }
 
 void system_updateButtons(uint32_t button_state, uint32_t has_changed)
@@ -31,10 +37,17 @@ void system_receiveUpdate(const uint8_t *data, uint32_t length)
 
 void system_updateUi(uint8_t connected)
 {
+    // Every 500ms
     if (connected)
+    {
         remote_updateUi();
+        connection = (connection > enableTimer) ? connection : (connection + 1);
+    }
     else
+    {
+        connection = 0;
         remote_disconnectedUi();
+    }
 }
 
 void system_setRssi(int8_t rssi)
